bf78a8885de3bee1af541b3d20d97d956f9e9ec4
Dueon Kim <dueonkim89@yahoo.com>
2017-05-22 00:26:28 -0700

    hamburger menu created, my pic added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 //initialize hamburger menu slide option
-$('.menu-link').bigSlide();
+$('.menu-link').bigSlide({
+	'menuWidth': '8rem'
+});
 
+
+var $viewport_width = $(window).width();
+
+if ($viewport_width >= 768) {
+	$('.menu-link').bigSlide({
+	'menuWidth': '0rem',
+	'state': 'open'
+	});
+
+}
 
